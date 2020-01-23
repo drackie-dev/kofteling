8e143eec063d10712e849450337218ed202de155
KijkEr <joep_is_cool@hotmail.com>
2020-01-23 13:43:41 +0100

    Fixed linting errors.

--- backend/application/config.py
+++ backend/application/config.py
@@ -1,5 +1,9 @@
+from urllib import parse
+
+
 class Config():
     pass
+
 
 postgress = True
 mssql = False
@@ -17,10 +21,9 @@ elif mssql:
         TESTING = True
         FLASK_DEBUG = True
         SECRET_KEY = 'geheim'
-        params = urllib.parse.quote_plus("DRIVER={SQL Server Native Client 11.0};SERVER=DESKTOP-N0GN0AT;DATABASE=KOFTELING;UID=kofteling;PWD=kofteling")
+        params = parse.quote_plus("DRIVER={SQL Server Native Client 11.0};SERVER=DESKTOP-N0GN0AT;DATABASE=KOFTELING;UID=kofteling;PWD=kofteling")
         SQLALCHEMY_DATABASE_URI = "mssql+pyodbc:///?odbc_connect=%s" % params
-        #SQLALCHEMY_DATABASE_URI = "mssql+pyodbc:///kofteling:kofteling@SQL_SERVER"
+        # SQLALCHEMY_DATABASE_URI = "mssql+pyodbc:///kofteling:kofteling@SQL_SERVER"
         SQLALCHEMY_TRACK_MODIFICATIONS = False
 
         # @127.0.0.1\KOFTELING?driver=ODBC+Driver+17+for+SQL+Server
-
--- backend/application/models.py
+++ backend/application/models.py
@@ -1,10 +1,11 @@
-from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import MetaData, Column
+# from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import MetaData
 from app import db
 
 meta = MetaData(schema='public')
 
-class Users(db.Model):
+
+class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     public_id = db.Column(db.String(50), unique=True)
